e5cfe78665afc3c4d63597e79bc5cffe0f7ad3d3
Diego Batista <diegosbatista89@gmail.com>
2020-07-04 12:08:01 -0300

    🎉 Initial commit with packages config and folder structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from '@emotion/styled/macro'
+import { keyframes } from '@emotion/core'
+import logo from './logo.svg'
+
+const SpinAnimation = keyframes`
+   from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${SpinAnimation} infinite 20s linear;
+  }
+`
+
+const LinkTag = styled.a`
+  color: #61dafb;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainer>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <LinkTag
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </LinkTag>
+      </Header>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
